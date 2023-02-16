0be8b344d28c3ac91a6cd73c28250f71df8763b1
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-02-16 15:38:45 -0800

    footer update

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   const currentYear = new Date(). getFullYear()
     return (
       <div  className='page-footer'>
-       <p>&copy; <span id='date'>{currentYear}</span> Built by <a href='https://github.com/YElnadi/DishDiva2.git' style={{textDecoration:'none', fontWeight:'bold'}}>Yasmine Elnadi</a></p>
+       <p>&copy; <span id='date'>{currentYear} </span> <a href='https://github.com/YElnadi/DishDiva2.git' style={{textDecoration:'none', fontWeight:'bold'}}>DishDiva</a> Built by <a href='https://www.linkedin.com/in/yasmine-elnadi-117912142/' style={{textDecoration:'none', fontWeight:'bold'}}>Yasmine Elnadi</a></p>
        
       </div>
     );
