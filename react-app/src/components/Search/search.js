3b1d4326aa3492df3bfa65f36bd5d0d7a1cd1fc1
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-07-13 03:11:58 -0700

    working on an error

--- react-app/src/components/Search/search.js
+++ react-app/src/components/Search/search.js
@@ -135,9 +135,11 @@ const Search = () => {
   };
 
   useEffect(() => {
-    setSearchInput(initialSearchInput);
-    fetchData(initialSearchInput);
-  }, [initialSearchInput]);
+    if (initialSearchInput) {
+        setSearchInput(initialSearchInput);
+        fetchData(initialSearchInput);
+      }
+    }, []);
 
   const handleChange = (value) => {
     setSearchInput(value);
@@ -177,3 +179,103 @@ const Search = () => {
 };
 
 export default Search;
+
+// import React, { useEffect, useState, useRef } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useHistory, useLocation } from "react-router-dom";
+// import "./search.css";
+// import SearchResultList from "../SearchResultList/SearchResultList";
+
+// const Search = () => {
+//   const dispatch = useDispatch();
+//   const history = useHistory();
+//   const location = useLocation();
+//   const [searchInput, setSearchInput] = useState("");
+//   const [results, setResults] = useState([]);
+//   const [searchShow, setSearchShow] = useState(false);
+//   const searchRef = useRef(null);
+
+//   const queryParams = new URLSearchParams(location.search);
+//   const initialSearchInput = queryParams.get("search") || "";
+
+//   const fetchData = (value) => {
+//     fetch("/api/recipes")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log("API response:", data);
+//         if (Array.isArray(data.recipes)) {
+//           const filteredResults = data.recipes.filter((recipe) => {
+//             return (
+//               value &&
+//               recipe &&
+//               recipe.title &&
+//               recipe.title.toLowerCase().includes(value.toLowerCase())
+//             );
+//           });
+
+//           setResults(filteredResults);
+//         } else {
+//           console.error(
+//             "API response does not contain an array of recipes:",
+//             data
+//           );
+//         }
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching data:", error);
+//       });
+//   };
+
+//   useEffect(() => {
+//     let isMounted = true;
+
+//     if (initialSearchInput) {
+//       setSearchInput(initialSearchInput);
+//       fetchData(initialSearchInput);
+//     }
+
+//     return () => {
+//       isMounted = false;
+//     };
+//   }, []);
+
+//   const handleChange = (value) => {
+//     setSearchInput(value);
+//     setSearchShow(true); // Set searchShow to true to open the list
+//     history.replace({
+//       pathname: location.pathname,
+//       search: value ? `?search=${value}` : "",
+//     });
+//     fetchData(value);
+//   };
+
+//   const handleInputBlur = () => {
+//     // Close the list when the search input loses focus
+//     setSearchShow(false); // Set searchShow to false to close the list
+//     setResults([]); // Clear the search results
+//   };
+
+//   return (
+//     <div className="search">
+//       <div className="search-bar-container">
+//         <div className="input-wrapper">
+//           <i className="fa-solid fa-magnifying-glass"></i>
+//           <input
+//             type="search"
+//             placeholder="What would you like to cook?"
+//             value={searchInput}
+//             onChange={(e) => handleChange(e.target.value)}
+//             onBlur={handleInputBlur}
+//           />
+//         </div>
+//       </div>
+//       {searchShow && results.length > 0 && (
+//         <div className="search-results-list-container">
+//           <SearchResultList results={results} />
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Search;
--- react-app/src/components/SearchResultList/SearchResultList.js
+++ react-app/src/components/SearchResultList/SearchResultList.js
@@ -3,25 +3,25 @@ import "./SearchResultList.css";
 import { useHistory } from "react-router-dom";
 
 const SearchResultList = ({ results }) => {
-    const history = useHistory()
-  const handleClick = (recipeId) => {
-    history.push(`/recipes/${recipeId}`);
-  };
-  return (
-    <div>
-      {results.map((result, id) => {
-        return (
+    const history = useHistory();
+  
+    const handleClick = (recipeId) => {
+      history.push(`/recipes/${recipeId}`);
+    };
+  
+    return (
+      <div>
+        {results.map((result) => (
           <div
             className="search-results-list"
-            key={id}
+            key={result.id}
             onClick={() => handleClick(result.id)}
           >
             {result.title}
           </div>
-        );
-      })}
-    </div>
-  );
-};
+        ))}
+      </div>
+    );
+  };
 
 export default SearchResultList;
