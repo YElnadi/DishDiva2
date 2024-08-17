de47c5e83298c33c2c8776fdf3fd1c2e1e879984
Yasmine Elnadi <yasmine_adel@hotmail.com>
2024-08-17 02:25:57 -0700

    add comment

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -27,3 +27,8 @@ class LoginForm(FlaskForm):
     email = StringField('email', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[
                            DataRequired(), password_matches])
+    
+# class LoginForm(FlaskForm):
+#     email = StringField('email', validators=[DataRequired(), user_exists])
+#     password = StringField('password', validators=[
+#                            DataRequired(), password_matches])
